9af2ef9b41e8d5f83b9c71d3932a202f07c1376a
Rafael Gibim <rafael.gibim@codeminer42.com>
2017-07-20 05:50:40 -0300

    Books created

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+  def index
+    @books = Book.all
+    respond_to do |f|
+      f.html
+      f.json { render json: @books, status: :ok }
+    end
+  end
+
+  def create
+    book = Book.new params.require(:book).permit(:author, :title, :year)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: { error: 'title_already_exists' }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @book = Book.find(params.fetch(:id))
+    respond_to do |f|
+      f.html
+      f.json { render json: @book }
+    end
+  end
+
+  def destroy
+    Book.find(params.fetch(:id)).destroy
+    head :no_content
+  end
+
+  private
+
+  def render_record_not_found
+    head :no_content
+  end
+end
